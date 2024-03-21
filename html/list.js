de874dc81ff827d89a5ea12b9162a9a9fdbbc571
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2024-03-21 20:08:30 +0800

    修正JS中文等宽判断失误的问题

--- html/list.js
+++ html/list.js
@@ -1,45 +1,51 @@
 const monospacedC = document.getElementById('monospaced');
 const fontChineseC = document.getElementById('fontChinese');
-const visibled = [0,0]
-function chVisible(classes,fontInfo, orNames) {
-    let isOK = false
-    for (let i = 0; i < orNames.length; i++) {
-        const orName = orNames[i];
+const visibled = [0, 0]
+function chVisible(classes, fontInfo, names, isOr = false) {
+    let isOK = 0
+    for (let i = 0; i < names.length; i++) {
+        const orName = names[i];
         if (classes.includes(orName)) {
-            isOK = true;
+            isOK++;
             break;
         }
     }
-    if (isOK) {
-		visibled[0]++
+    // console.log("names",names) //['monospaced', 'zh_monospaced']
+    // console.log("classes",classes) //['fontMonospaced', 'monospaced', 'fontChinese', 'zh_monospaced']
+    if ((isOr && isOK > 0) || (!isOr && isOK == names.length)) {
+        visibled[0]++
         fontInfo.style.display = '';
     } else {
-		visibled[1]++
+        visibled[1]++
         fontInfo.style.display = 'none';
     }
 }
 function chView() {
-	visibled[0] = 0
-	visibled[1] = 0
-    const monospaced = monospacedC.checked;
-    const fontChinese = fontChineseC.checked;
+    visibled[0] = 0
+    visibled[1] = 0
+    const isMonospaced = monospacedC.checked;
+    const isChinese = fontChineseC.checked;
     const fontInfos = document.getElementsByClassName('fontInfo');
     for (let i = 0; i < fontInfos.length; i++) {
         const fontInfo = fontInfos[i];
         const fontMonospaced = fontInfo.getElementsByClassName('fontMonospaced')[0];
-		const fontChinese = fontInfo.getElementsByClassName('fontChinese')[0];
+        const fontChinese = fontInfo.getElementsByClassName('fontChinese')[0];
         const classes = fontMonospaced.className.split(' ').concat(fontChinese.className.split(' '));
-        if (monospaced) {
-            chVisible(classes,fontInfo, ['monospaced'])
-        } else {
-            if (fontChinese) {
-                chVisible(classes,fontInfo, ['zh','zh_monospaced'])
+        if (isMonospaced) {
+            if (isChinese) {
+                chVisible(classes, fontInfo, ['zh_monospaced'])
             } else {
-                fontInfo.style.display = 'block';
+                chVisible(classes, fontInfo, ['monospaced'])
+            }
+        } else {
+            if (isChinese) {
+                chVisible(classes, fontInfo, ['zh', 'zh_monospaced'], true)
+            } else {
+                fontInfo.style.display = '';
             }
         }
     }
-	console.log(monospaced,fontChinese,visibled);
+    console.log(isMonospaced, isChinese, visibled);
 }
 monospaced.addEventListener('change', () => {
     chView();
@@ -49,5 +55,5 @@ fontChinese.addEventListener('change', () => {
 });
 window.addEventListener('load', () => {
     monospacedC.disabled = '';
-	fontChineseC.disabled = '';
+    fontChineseC.disabled = '';
 });
