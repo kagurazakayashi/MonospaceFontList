ba18aa76606ec7f83e4ffdd0229b9d2299a6dcac
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2024-03-22 20:45:00 +0800

    新增字体 DPI 和 Hinting 配置

--- main.go
+++ main.go
@@ -33,6 +33,8 @@ var (
 	chineseChkLevel  int
 	monospacedTotal  uint = 0
 	fontSize         float64
+	fontDPI          float64 = 72
+	fontHinting      int
 	outDir           string
 	imageTop         int
 	imageLeft        int
@@ -50,17 +52,19 @@ func main() {
 	flag.StringVar(&scanDir, "i", "", "要扫描的字体文件夹，默认为系统字体文件夹。")
 	flag.StringVar(&extensionNames, "e", "ttf,otf,ttc", "要扫描的字体文件扩展名，用 `,` 分隔。默认为 `ttf,otf,ttc` 。")
 	flag.StringVar(&enTestChars, "en", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789", "英文测试字符列表，默认大小写字母和数字。")
-	flag.StringVar(&zhTestChars, "zt", "正，一丨中！小。", "中文测试字符列表。")
-	flag.IntVar(&chineseChkLevel, "zh", 7, "中文 Unicode 区块检查级别( 0.基本 - 7.扩展 )")
+	flag.StringVar(&zhTestChars, "chst", "正，一丨中！小。", "中文测试字符列表。")
+	flag.IntVar(&chineseChkLevel, "chsv", 7, "中文 Unicode 区块检查级别( 0:基本 ~ 7(默认):扩展 )")
 	flag.StringVar(&outDir, "o", "out", "输出文件夹，不带结尾 `/` 。默认为 `当前文件夹/out` 。")
 	flag.Float64Var(&fontSize, "s", 64, "字体预览图所使用的字体大小")
-	flag.IntVar(&imageLeft, "x", 10, "字体预览图左侧空白")
-	flag.IntVar(&imageTop, "y", 64, "字体预览图顶部空白")
-	flag.IntVar(&imageWidth, "w", 800, "字体预览图宽度")
-	flag.IntVar(&imageHeight, "h", 78, "字体预览图高度")
+	flag.Float64Var(&fontDPI, "d", 72, "字体预览图所使用的字体 DPI, 默认为 72 。")
+	flag.IntVar(&fontHinting, "hi", 0, "字体预览图所使用的字体 Hinting。 0(默认):无; 1:垂直; 2:全部。")
+	flag.IntVar(&imageLeft, "ix", 10, "字体预览图左侧空白")
+	flag.IntVar(&imageTop, "iy", 64, "字体预览图顶部空白")
+	flag.IntVar(&imageWidth, "iw", 800, "字体预览图宽度")
+	flag.IntVar(&imageHeight, "ih", 78, "字体预览图高度")
 	flag.StringVar(&imageText, "t", "AaBbCc0123?.文字。", "字体预览图文字")
 	flag.StringVar(&pageTitle, "p", "MonospaceFontList", "网页标题前缀")
-	flag.IntVar(&maxGoroutines, "j", 0, "最大并发数，默认为逻辑处理器数量。")
+	flag.IntVar(&maxGoroutines, "j", 0, "最大并发数。 0(默认):按逻辑处理器数量; 1:单线程; >1:启用多线程。")
 	flag.Parse()
 
 	if maxGoroutines <= 0 {
@@ -74,7 +78,9 @@ func main() {
 		os.Exit(1)
 	}
 	fileList()
-	log.Println("并发:", maxGoroutines)
+	if maxGoroutines > 1 {
+		log.Println("并发:", maxGoroutines)
+	}
 	fontInfo()
 	saveHTML()
 	// outJSONInfo()
